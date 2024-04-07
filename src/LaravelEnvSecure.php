28ee3c74b4219071c3df4f1654009b0975495e09
izica <artem@redlg.ru>
2024-04-08 01:02:19 +0700

    Update main.yml, .styleci.yml, CHANGELOG.md, and 8 more files

---
+++ src/LaravelEnvSecure.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Izica\LaravelEnvSecure;
+
+class LaravelEnvSecure
+{
+    // Build your next great package.
+}
---
+++ src/LaravelEnvSecureFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Izica\LaravelEnvSecure;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Izica\LaravelEnvSecure\Skeleton\SkeletonClass
+ */
+class LaravelEnvSecureFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-env-secure';
+    }
+}
