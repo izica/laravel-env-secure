8b2cdb6f1cebfd95b57d8f5715c45966e99cce1c
izica <artem@redlg.ru>
2024-04-08 02:09:53 +0700

    Update LaravelEnvSecureServiceProvider.php

--- src/LaravelEnvSecureServiceProvider.php
+++ src/LaravelEnvSecureServiceProvider.php
@@ -21,7 +21,7 @@ class LaravelEnvSecureServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('env-secure.php'),
+                __DIR__.'/../config/env-secure.php' => config_path('env-secure.php'),
             ], 'config');
 
             // Publishing the views.
@@ -50,7 +50,7 @@ class LaravelEnvSecureServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-env-secure');
+        $this->mergeConfigFrom(__DIR__.'/../config/env-secure.php', 'env-secure');
 
         // Register the main class to use with the facade
         $this->app->singleton('laravel-env-secure', function () {
